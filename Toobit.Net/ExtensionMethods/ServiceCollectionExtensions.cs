19bc814b76b3e0cb6f7cbaa97ee1656fecfaad30
Jkorf <jankorf91@gmail.com>
2025-09-01 13:54:26 +0200

    Updated CryptoExchange.Net to v9.7.0

--- Toobit.Net/ExtensionMethods/ServiceCollectionExtensions.cs
+++ Toobit.Net/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -94,25 +94,8 @@ namespace Microsoft.Extensions.DependencyInjection
                 client.Timeout = options.RequestTimeout;
                 return new ToobitRestClient(client, serviceProvider.GetRequiredService<ILoggerFactory>(), serviceProvider.GetRequiredService<IOptions<ToobitRestOptions>>());
             }).ConfigurePrimaryHttpMessageHandler((serviceProvider) => {
-                var handler = new HttpClientHandler();
-                try
-                {
-                    handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-                    handler.DefaultProxyCredentials = CredentialCache.DefaultCredentials;
-                }
-                catch (PlatformNotSupportedException)
-                { }
-
                 var options = serviceProvider.GetRequiredService<IOptions<ToobitRestOptions>>().Value;
-                if (options.Proxy != null)
-                {
-                    handler.Proxy = new WebProxy
-                    {
-                        Address = new Uri($"{options.Proxy.Host}:{options.Proxy.Port}"),
-                        Credentials = options.Proxy.Password == null ? null : new NetworkCredential(options.Proxy.Login, options.Proxy.Password)
-                    };
-                }
-                return handler;
+                return LibraryHelpers.CreateHttpClientMessageHandler(options.Proxy, options.HttpKeepAliveInterval);
             });
             services.Add(new ServiceDescriptor(typeof(IToobitSocketClient), x => { return new ToobitSocketClient(x.GetRequiredService<IOptions<ToobitSocketOptions>>(), x.GetRequiredService<ILoggerFactory>()); }, socketClientLifeTime ?? ServiceLifetime.Singleton));
 
